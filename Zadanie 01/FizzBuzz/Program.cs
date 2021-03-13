231dbafb7ee4e54fdb05792b3151c1c8cbd54930
Krystian Szmigielski <krystian.szmigielski2@gmail.com>
2021-03-13 14:00:44 +0100

    Initial commit

---
+++ Zadanie 01/FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Podaj liczbę: ");
+                    var number = int.Parse(Console.ReadLine());
+                    string result = "";
+                    if (number % 3 == 0)
+                        result += "Fizz";
+                    if (number % 5 == 0)
+                        result += "Buzz";
+
+                    result = string.IsNullOrEmpty(result) ? number.ToString() : result;
+                    Console.WriteLine(result);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Musisz podać liczbę całkowitą!");
+                }
+            }
+        }
+    }
+}
---
+++ Zadanie 02/Forms/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zadanie2.Forms
+{
+    public class Address
+    {
+        [Display(Name = "Moja ulica"), Required(ErrorMessage = "Musisz podać ulicę!")]
+        public string Street { get; set; }
+        [Display(Name = "Kod pocztowy"), Required(ErrorMessage = "Musisz podać kod pocztowy!")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Miasto"), Required(ErrorMessage = "Musisz podać miasto!")]
+        public string City { get; set; }
+        [Display(Name = "Numer"), Required(ErrorMessage = "Musisz podać numer domu!")]
+        public int Number { get; set; }
+
+    }
+}
