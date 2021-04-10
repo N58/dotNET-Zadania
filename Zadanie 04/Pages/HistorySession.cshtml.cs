2e9fe2b9f7659b38a4d38c7649e254a68f469313
Krystian Szmigielski <krystian.szmigielski2@gmail.com>
2021-04-10 17:36:07 +0200

    Added Zadanie 04

---
+++ Zadanie 04/Pages/HistorySession.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Zadanie_4.Forms;
+
+namespace Zadanie_3.Pages
+{
+    public class HistorySessionModel : PageModel
+    {
+        public IList<FizzNumber> NumbersList { get; set; }
+
+        public void OnGet()
+        {
+            var numbersList = HttpContext.Session.GetString("NumbersList");
+
+            if (numbersList != null)
+                NumbersList = JsonConvert.DeserializeObject<IList<FizzNumber>>(numbersList);
+        }
+    }
+}
---
+++ Zadanie 04/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zadanie_4.Forms;
+
+namespace Zadanie_3
+{
+    public static class Utils
+    {
+        public static string GetFizzString(FizzNumber fizz)
+        {
+            // For none: result == 0
+            // For 3: result == 1
+            // For 5: result == 2
+            // For 3 and 5: result == 3
+            return fizz.Result switch
+            {
+                0 => "Liczba " + fizz.Number + " nie spełnia kryteriów Fizz/Buzz",
+                1 => "Fizz",
+                2 => "Buzz",
+                3 => "FizzBuzz",
+                _ => "Error",
+            };
+        }
+    }
+}
