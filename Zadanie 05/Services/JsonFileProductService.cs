ca13a0b5874f329a47b97608a46c32faa3adc0b7
Krystian Szmigielski <krystian.szmigielski2@gmail.com>
2021-04-10 21:06:41 +0200

    Added not finished Zadanie 05

---
+++ Zadanie 05/Services/JsonFileProductService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Zadanie_5.Models;
+
+namespace Zadanie_5.Services
+{
+    public class JsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
