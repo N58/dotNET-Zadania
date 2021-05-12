8e331a8185ad4ea3b10126c57359b0e0ade6cb21
Krystian Szmigielski <krystian.szmigielski2@gmail.com>
2021-05-12 18:05:57 +0200

    Added Zadanie 08

---
+++ Zadanie 08/Utils/CustomFilterAttributes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Zadanie_08.Utils
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            var result = context.Result;
+
+            if (result is PageResult)
+            {
+                var page = ((PageResult)result);
+                page.ViewData["ip"] = context.HttpContext.Connection.RemoteIpAddress.MapToIPv4();
+            }
+
+        }
+    }
+}
